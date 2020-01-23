d9ab854a9065344826cbcfe44fe74bd2c230aa57
Martin <gavadianmartin@gmail.com>
2020-01-23 17:12:58 +0100

    metiendo menu

--- app/src/main/java/com/example/blognotasampliado/MainActivity.java
+++ app/src/main/java/com/example/blognotasampliado/MainActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
@@ -23,17 +25,42 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    public void crear(View v){
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true; /** true -> el menú ya está visible */
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        int id = item.getItemId();
+        if (id == R.id.crearNota) {
+            crear();
+            return true;
+        }
+        if (id == R.id.verNota) {
+            ver();
+            return true;
+        }
+        if (id == R.id.eliminarNota) {
+            eliminar();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    public void crear(){
         Intent intent = new Intent (this, CrearNota.class);
         startActivity(intent);
     }
 
-    public void ver(View v){
+    public void ver(){
         Intent intent = new Intent (this, VerNota.class);
         startActivity(intent);
     }
 
-    public void eliminar(View v){
+    public void eliminar(){
         Intent intent = new Intent (this, EliminarNota.class);
         startActivity(intent);
     }
